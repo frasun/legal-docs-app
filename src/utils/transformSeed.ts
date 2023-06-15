a776476a7b0d7ed6cd3ccc1d97500e2427639bf8
fraz <fraz@o2.pl>
2023-06-15 10:15:58 +0200

    fix formatting and encrypt data in document

--- src/utils/transformSeed.ts
+++ src/utils/transformSeed.ts
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import { TODAY, DATE_FORMAT } from "./cosntants";
+import { TODAY, DATE_FORMAT } from "./constants";
 
 export default function (seed) {
   for (let [key, value] of Object.entries(seed)) {
