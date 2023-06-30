8b098004b40f75989de1874403c3425e33d6a6f2
fraz <fraz@o2.pl>
2023-06-30 09:06:09 +0200

    play with cryptr salt length

--- src/utils/crypt.ts
+++ src/utils/crypt.ts
@@ -1,2 +1,2 @@
 import Cryptr from "cryptr";
-export default new Cryptr(import.meta.env.CRYPTR_SECRET, { saltLength: 10 });
+export default new Cryptr(import.meta.env.CRYPTR_SECRET, { saltLength: 5 });
