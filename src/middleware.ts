34b3dec55be65374732c24aad7989bcf645949dc
fraz <fraz@o2.pl>
2023-07-26 13:11:16 +0200

    test images

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,11 @@ const SESSION_COOKIE = "__SSID";
 
 export const onRequest = defineMiddleware(
   async ({ cookies, request }, next) => {
-    if (import.meta.env.MODE === "production") {
-      const url = new URL(request.url);
-      const baseUrl = url.origin;
-      return Response.redirect(`${baseUrl}/404`, 302);
-    }
+    // if (import.meta.env.MODE === "production") {
+    //   const url = new URL(request.url);
+    //   const baseUrl = url.origin;
+    //   return Response.redirect(`${baseUrl}/404`, 302);
+    // }
 
     if (!cookies.has(SESSION_COOKIE)) {
       cookies.set(SESSION_COOKIE, nanoid(), {
