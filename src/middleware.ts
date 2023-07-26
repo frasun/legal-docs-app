31b6bf9f2b8faf82440cdc404d8effad7543b045
fraz <fraz@o2.pl>
2023-07-26 13:32:01 +0200

    revert production redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,15 @@ import { nanoid } from "nanoid";
 
 const SESSION_COOKIE = "__SSID";
 
-export const onRequest = defineMiddleware(
-  async ({ cookies, request }, next) => {
-    // if (import.meta.env.MODE === "production") {
-    //   const url = new URL(request.url);
-    //   const baseUrl = url.origin;
-    //   return Response.redirect(`${baseUrl}/404`, 302);
-    // }
-
-    if (!cookies.has(SESSION_COOKIE)) {
-      cookies.set(SESSION_COOKIE, nanoid(), {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: import.meta.env.MODE === "production",
-        path: "/",
-      });
-    }
-
-    return next();
+export const onRequest = defineMiddleware(async ({ cookies }, next) => {
+  if (!cookies.has(SESSION_COOKIE)) {
+    cookies.set(SESSION_COOKIE, nanoid(), {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: import.meta.env.MODE === "production",
+      path: "/",
+    });
   }
-);
+
+  return next();
+});
