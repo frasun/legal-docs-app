d174657571fa188e900768213a30046346fd8cab
fraz <fraz@o2.pl>
2023-08-20 12:52:41 +0200

    use short names for url params

--- src/utils/urlParams.ts
+++ src/utils/urlParams.ts
@@ -1,1 +1,7 @@
 export const EMAIL = "e";
+export const REDIRECT = "r";
+export const DOCUMENT = "d";
+export const DRAFT = "dr";
+export const CATEGORY = "k";
+export const SEARCH = "s";
+export const PAGE = "p";
