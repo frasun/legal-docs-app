ea4466dd7b86420ddd013227a324e2830dcd2014
fraz <fraz@o2.pl>
2023-09-10 11:36:13 +0200

    refactor toasts to use server-side js instead of url params

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,19 +1,4 @@
-const toasts = [
-  {
-    param: "updt",
-    info: "Odpowiedź została zapisana",
-  },
-  {
-    param: "namch",
-    info: "Nazwa dokumentu została zmieniona",
-  },
-  {
-    param: "drsvd",
-    info: "Szkic dokumentu został zapisany",
-  },
-];
-
-export default toasts;
-export const UPDATED_ANSWER_PARAM = toasts[0].param;
-export const DOCUMENT_NAME_CHANGED_PARAM = toasts[1].param;
-export const DRAFT_SAVED_PARAM = toasts[2].param;
+export const DOCUMENT_NAME_CHANGED = "Nazwa dokumentu została zmieniona";
+export const ANSWER_UPDATED = "Odpowiedź została zapisana";
+export const DRAFT_SAVED_PARAM = "draft-saved";
+export const DRAFT_SAVED = "Szkic dokumentu został zapisany";
