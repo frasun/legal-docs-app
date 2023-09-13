500679930a2604f3b6ee1278e4244cab046847fe
fraz <fraz@o2.pl>
2023-09-13 12:23:31 +0200

    move to mongodb atlas

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -1,7 +1,13 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const client = new MongoClient(import.meta.env.MONGODB_URI);
-// const database = client.db("prawniczek");
+const client = new MongoClient(import.meta.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  compressors: ["zlib"],
+});
 const database = client.db(import.meta.env.MONGODB_DBNAME);
 
 export default database;
