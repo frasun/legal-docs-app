ec09eed8b9c2bd8234ae44e134900742a686ea97
fraz <fraz@o2.pl>
2023-09-15 08:48:45 +0200

    prefetch documents page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import image from "@astrojs/image";
 import compress from "astro-compress";
 import vercel from '@astrojs/vercel/serverless';
 import { loadEnv } from "vite";
-
 import mdx from "@astrojs/mdx";
 import auth from "auth-astro";
+import prefetch from "@astrojs/prefetch";
 
 // populate env vars to process.env *astro hack
 const localEnv = loadEnv(import.meta.env.MODE, process.cwd(), "");
 for (let [key, value] of Object.entries(localEnv)) {
   process.env[key] = value;
 }
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,7 +25,9 @@ export default defineConfig({
   }), image(), compress({
     img: false,
     svg: false
-  }), mdx(), auth()],
+  }), mdx(), auth(), prefetch({
+    intentSelector: ['a[href$="/dokumenty"]', 'a[href*="/dokumenty?"]', 'a[href$="/podsumowanie"]', 'a[href$="/dokument"]'],
+  })],
   output: 'server',
   adapter: vercel()
 });
