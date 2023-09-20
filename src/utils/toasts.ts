b955d4ada9ce209d20d4f117f2c2986cadc1fbfd
fraz <fraz@o2.pl>
2023-09-20 16:17:31 +0200

    use prg pattern to limit multiple form submissions

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,4 +1,6 @@
 export const DOCUMENT_NAME_CHANGED = "Nazwa dokumentu została zmieniona";
+export const DOCUMENT_NAME_PARAM = "name-changed";
+export const ANSWER_UPDATED_PARAM = "answer-updated";
 export const ANSWER_UPDATED = "Odpowiedź została zapisana";
 export const DRAFT_SAVED_PARAM = "draft-saved";
 export const DRAFT_SAVED = "Szkic dokumentu został zapisany";
