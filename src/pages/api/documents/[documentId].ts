9087312d2027cf365ca99cd0cf12ab00dedefcac
fraz <fraz@o2.pl>
2023-10-05 18:07:33 +0200

    use endpoint to get document info

--- src/pages/api/documents/[documentId].ts
+++ src/pages/api/documents/[documentId].ts
@@ -2,6 +2,103 @@ import { UNAUTHORIZED } from "@utils/response";
 import type { APIRoute } from "astro";
 import { getSession } from "auth-astro/server";
 import { deleteDraft } from "@db/document";
+import { UserRoles } from "@db/user";
+//@ts-ignore
+import { sanityClient } from "sanity:client";
+import { getEntry } from "astro:content";
+import Stripe from "stripe";
+import type { Document } from "@type";
+
+export const get: APIRoute = async ({ request, params }) => {
+  const { documentId } = params;
+  const session = await getSession(request);
+  const isAdmin = session?.user?.role === UserRoles.admin;
+
+  const allPostQuery = `_type=='post' && references(^._id) && defined(publishedAt)`;
+  const postQuery = session
+    ? allPostQuery
+    : `${allPostQuery} && (memberContent == false || !defined(memberContent))`;
+
+  const info: Document = await sanityClient.fetch(
+    `*[_type == 'legalDocument' && slug.current == "${documentId}"] { 
+      title, 
+      "draft": !publishedAt, 
+      body,
+      memberContent,
+      keywords,
+      description,
+      "posts": *[${postQuery}] | order(publishedAt desc) [0..2] { 
+        title, 
+        mainImage, 
+        publishedAt,
+        slug,
+        "excerpt": array::join(string::split((pt::text(body)), "")[0..255], "") + "..."
+      }
+  }[0]`
+  );
+
+  if (!info) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  const { title, body, draft, memberContent, keywords, description, posts } =
+    info;
+
+  if (draft) {
+    if (!session || isAdmin) {
+      return new Response(null, {
+        status: 404,
+      });
+    }
+  }
+
+  const document = await getEntry("documents", documentId as string);
+
+  if (!document) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  const { index, priceId } = document.data;
+
+  if (!index) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  if (memberContent && !session) {
+    return new Response(null, {
+      status: 401,
+    });
+  }
+
+  const stripe = new Stripe(import.meta.env.STRIPE_API_KEY, {
+    apiVersion: import.meta.env.STRIPE_API_V,
+  });
+
+  const documentPrice = await stripe.prices.retrieve(priceId);
+  const price = documentPrice.unit_amount ? documentPrice.unit_amount / 100 : 0;
+
+  const firstQuestionUrl = `/dokumenty/${documentId}/${index[0].questions[0].id.slug}`;
+
+  const response = {
+    title,
+    keywords,
+    description,
+    price,
+    firstQuestionUrl,
+    body,
+    posts,
+  };
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+  });
+};
 
 export const all: APIRoute = async ({ params, request }) => {
   const session = await getSession(request);
@@ -22,7 +119,7 @@ export const all: APIRoute = async ({ params, request }) => {
       });
     } catch (e) {
       return new Response(e instanceof Error ? e.message : null, {
-        status: 400,
+        status: 500,
       });
     }
   }
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,22 @@
+import type { ImageAsset, Slug } from "@sanity/types";
+import type { PortableTextBlock } from "@portabletext/types";
+
 export type Answers = Record<string, any>;
+
+export interface PostShort {
+  title: string;
+  publishedAt: string;
+  slug: Slug;
+  mainImage?: ImageAsset;
+  excerpt: string;
+}
+
+export interface Document {
+  title: string;
+  draft: string;
+  body: PortableTextBlock[];
+  memberContent: boolean;
+  keywords?: string;
+  description?: string;
+  posts: PostShort[];
+}
