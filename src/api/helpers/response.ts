ecf919fb8fbae417c0002f5557b1157282cb9271
fraz <fraz@o2.pl>
2023-11-27 16:32:54 +0100

    add security headers to api response

--- src/api/helpers/response.ts
+++ src/api/helpers/response.ts
@@ -3,6 +3,10 @@ import { API_URL } from "@api/helpers/url";
 export const responseHeaders = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": API_URL,
+  "Content-Security-Policy": "default-src 'self'",
+  "X-Frame-Options": "DENY",
+  "Strict-Transport-Security": "max-age=86400; includeSubDomains",
+  "X-XSS-Protection": "1",
 };
 
 export function getStatus(e: Error) {
